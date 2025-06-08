1607b4aef24c3219ad5acb57f195fe408ae8511a
Daniil Sydorov <daniil.sydorov@nure.ua>
2025-06-08 18:38:22 +0300

    Delete Lab4/smp-pzpi-23-3-sydorov-daniil-lab4 directory

--- Lab4/smp-pzpi-23-3-sydorov-daniil-lab4/remove_from_cart.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-if (isset($_POST["product_id"])) {
-    $id = (int) $_POST["product_id"];
-    unset($_SESSION["cart"][$id]);
-    file_put_contents("cart.json", json_encode($_SESSION["cart"], JSON_PRETTY_PRINT));
-}
-header("Location: productsbin.php");
-exit;
